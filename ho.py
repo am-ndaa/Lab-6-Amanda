74aac260670a6810c713701701c1aad9d0ec2fa4
amand <amandatantalean1012@gmail.com>
2023-10-26 18:57:36 -0400

    im encoding

--- ho.py
+++ ho.py
@@ -1,2 +1,14 @@
 # Amanda
 print('This is mine')
+# making encoder
+
+def encoder(password):
+    password = str(password)
+    new_pass_list = []
+    for num in password:
+        num = int(num)
+        num += 3
+        new_pass_list.append(str(num))
+    new_pass = "".join(new_pass_list)
+    return new_pass
+
